b6c0e81af9c11346f6b55f90d523f484cf36c2ae
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-08-23 23:52:46 +0300

    components build()

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const About = () => {
+const Navbar = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const About = () => {
   )
 }
 
-export default About
+export default Navbar

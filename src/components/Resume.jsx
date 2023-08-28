5f8b054d701e3d13b967f6fdd289c6f69929aeee
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-08-28 15:44:56 +0300

    Resume page buildup and styling

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,40 +1,99 @@
 import React from 'react';
-import codeImage from '../icons/code.svg';
-import designImage from '../icons/design.svg';
-import teacherImage from '../icons/teacher.svg';
 
 const Resume = () => {
   return (
     <div className="resume-container">
-      <div className="resume-section">
+      <div className="resume-card">
         <h2>My Resume</h2>
-        <div className="resume-item">
-          <img src={codeImage} alt="Code Icon" />
-          <div className="resume-details">
-            <h3>Web Development</h3>
-            <p>
-              Highly motivated full stack Developer with a passion for creating innovative solutions. Experienced in creating web applications using React.js and currently learning Next.js.
-            </p>
+
+        {/* Education Section */}
+        <div className="resume-education-section">
+          <h3>Education</h3>
+          <div className="resume-item">
+            <p>Feb - August 2023</p>
+            <h4> Software Development</h4>
+            <p>Moringa School</p>
+          </div>
+
+          <div className="resume-item">
+            <p>2017-2021</p>
+            <h4> Bachelors of Education</h4>
+            <p> Kenyatta University</p>
           </div>
         </div>
-        <div className="resume-item">
-          <img src={designImage} alt="Design Icon" />
-          <div className="resume-details">
-            <h3>UI/UX</h3>
-            <p>
-              Skilled in designing captivating and user-friendly interfaces using tools like Figma. Strong ability to create engaging user experiences.
-            </p>
+
+        {/* Work Experience Section */}
+        <div className="resume-work-section">
+          <h3>Experience</h3>
+          <div className="resume-item">
+            <h4>Mission Star Academy</h4>
+            <p>Teacher Assistant</p>
+            <p>March - August 2022</p>
+          </div>
+
+          <div className="resume-item">
+            <h4>Pumwani Boys Secondary School</h4>
+            <p>Teacher Intern</p>
+            <p>October - December 2021</p>
           </div>
         </div>
-        <div className="resume-item">
-          <img src={teacherImage} alt="Teaching Icon" />
-          <div className="resume-details">
-            <h3>Teaching</h3>
-            <p>
-              Trained teacher of Music and Kiswahili. Experienced in sharing knowledge with young learners and fostering a positive learning environment.
-            </p>
-          </div>
+
+        {/* Skills Section */}
+        <div className="resume-section">
+          <h3>Work Skills</h3>
+          <ul>
+            <li>Web Development (JavaScript, React.js, Next.js,HTML5, CSS3, Bootstrap)</li>
+            <li>UI/UX Design (Figma)</li>
+            <li>Backend Development( SQL, PostgreSQL, Ruby, Ruby on Rails)</li>
+            <li>Teaching (Music and Kiswahili)</li>
+            <li>Version Control</li>
+          </ul>
         </div>
+
+        <div className="resume-section">
+          <h3>Soft Skills</h3>
+          <ul>
+            <li>Project Management</li>
+            <li>Product Pitching</li>
+            <li>Team collaboration</li>
+            <li>Communication</li>
+            <li>Problem solving</li>
+            <li>Professionalism</li>
+          </ul>
+        </div>
+         {/* contact Section */}
+        <div className='resume-contact-section'>
+          <h3>Contact Persons (References)</h3>
+          <ul className='resume-list'>
+            <li className='resume-item'>
+              <h4>David Muigai</h4>
+              <p>Lecturer, Kenyatta University</p>
+              <p> Email:<a href="mailto:damuigai@gmail.com">damuigai@gmail.com</a></p>
+              <p>Phone: +254 719 533 546</p>
+            </li>
+
+            <li className='resume-item'>
+            <h4>Maureen Murimi</h4>
+              <p>Technical Mentor, Moringa School</p>
+              <p> Email:<a href="mailto:murimimaureen8@gmail.com">murimimaureen8@gmail.com</a></p>
+              <p>Phone: +254 799 280 333</p>
+            </li>
+
+            <li className='resume-item'>
+            <h4>Regina Mwanyumba</h4>
+              <p>Director, Mission Star Academy</p>
+              <p> Email:<a href="mailto:mwanyumbaregina@gmail.com">mwanyumbaregina@gmail.com</a></p>
+              <p>Phone: +254 705 956 723</p>
+            </li>
+
+            <li className='resume-item'>
+            <h4> Mr Mwangi</h4>
+              <p>Principal, Pumwani Boys Secondary School</p>
+              <p>Phone: +254 724 046 623</p>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   );

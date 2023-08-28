ac1a6122961695129749a83836b96c6edfcfb782
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-08-28 12:53:48 +0300

    recreating the about and resume sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         <div className='about-section'>
           <h1>About Me</h1>
           <p>
-            I am a highly motivated full stack Developer and a teacher with a passion for creating innovative solutions. As a detail-oriented individual, I thrive in collaborative settings and enjoy working with diverse teams to tackle problems. Aside from the profesions, I like being in the kitchen trying out recipes and other culinary things and producing some food products. I am also an outdoor person, moreso camping and swimming.
+          I am an enthusiastic and dedicated full stack Developer and Educator, driven by a fervor for crafting inventive solutions. My attention to detail is second nature, and I thrive within collaborative environments, reveling in the opportunity to collaborate with diverse teams to solve challenges. Beyond my professional pursuits, I find solace in culinary experiments and recipe explorations within the kitchen, resulting in delightful culinary creations. My affinity for the outdoors compels me to embrace activities such as camping,travelling and swimming. As you journey through my profile, you'll discover the amalgamation of my technical prowess, teaching endeavors, and my diverse range of passions.
           </p>
         </div>
       </div>
@@ -20,27 +20,28 @@ const About = () => {
       </div>
     </div>
 
+     
      <div className='card-content'>
-     <h2>What I do</h2>
-     </div>
-   <div className='card-container'>
-   <div className='card'>
-   <h4 className='card-title'><img src={codeImage} alt="codeImage" /> Web Development</h4>
-   <p>I am captivated in creating web applications using React.js. I am in the process of learning Next.js</p>
-   </div>
+  <h2>What I Specialize In</h2>
+</div>
+<div className='card-container'>
+  <div className='card'>
+    <h4 className='card-title'><img src={codeImage} alt="codeImage" /> Crafting Digital Experiences</h4>
+    <p>My fascination lies in shaping captivating web applications using React.js, where I can seamlessly merge technology with creativity. Currently, I'm also exploring the realms of Next.js.</p>
+  </div>
 
-   <div className='card'>
-   <h4 className='card-title'><img src={designImage} alt="codeImage" />  UI/UX </h4>
-   <p>Designing captivating and user friendly interfaces is something I am truly passionate about when using Figma. </p>
-   </div>
+  <div className='card'>
+    <h4 className='card-title'><img src={designImage} alt="codeImage" /> Designing Intuitive UI/UX</h4>
+    <p>Delving into the world of UI/UX design is my passion. Creating interfaces that effortlessly guide users and leave a lasting impression is my way of merging aesthetics with functionality, using tools like Figma to bring visions to life.</p>
+  </div>
 
-   <div className='card'>
-   <h4 className='card-title'><img src={teacherImage} alt="codeImage" />Teaching</h4>
-   <p>Being a trained teacher of Music and Kiswahili, I have found great joy in sharing my knowledge with young ones. </p>
-   </div>
-   </div>
-  
-   </div>
+  <div className='card'>
+    <h4 className='card-title'><img src={teacherImage} alt="codeImage" />Sharing Knowledge as a Teacher</h4>
+    <p>As a trained educator in Music and Kiswahili, I have discovered immense satisfaction in imparting knowledge to young minds. Guiding them through the realm of learning and igniting curiosity is my way of nurturing future talents.</p>
+    </div>
+  </div>
+</div>
+
   )
 }
 
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import codeImage from '../icons/code.svg';
+import designImage from '../icons/design.svg';
+import teacherImage from '../icons/teacher.svg';
 
 const Resume = () => {
   return (
-    <div>
-      
+    <div className="resume-container">
+      <div className="resume-section">
+        <h2>My Resume</h2>
+        <div className="resume-item">
+          <img src={codeImage} alt="Code Icon" />
+          <div className="resume-details">
+            <h3>Web Development</h3>
+            <p>
+              Highly motivated full stack Developer with a passion for creating innovative solutions. Experienced in creating web applications using React.js and currently learning Next.js.
+            </p>
+          </div>
+        </div>
+        <div className="resume-item">
+          <img src={designImage} alt="Design Icon" />
+          <div className="resume-details">
+            <h3>UI/UX</h3>
+            <p>
+              Skilled in designing captivating and user-friendly interfaces using tools like Figma. Strong ability to create engaging user experiences.
+            </p>
+          </div>
+        </div>
+        <div className="resume-item">
+          <img src={teacherImage} alt="Teaching Icon" />
+          <div className="resume-details">
+            <h3>Teaching</h3>
+            <p>
+              Trained teacher of Music and Kiswahili. Experienced in sharing knowledge with young learners and fostering a positive learning environment.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;

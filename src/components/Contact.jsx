8b984e36fa7dcb0acce1648ee0d2be71a0190eb3
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-08-29 12:05:17 +0300

    contact page build and style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission or validation here
+    // You can send the form data to a backend API, for example
+  };
 
-export default Contact
+  return (
+    <div className="contact-container">
+      {/* Contact Details Cards */}
+      <div className="contact-cards">
+        <div className="contact-card">
+          <h3 className="bi bi-telephone">Phone</h3>
+          <p>+254 710 482 080</p>
+        </div>
+        <div className="contact-card">
+          <h3 className="bi bi-envelope">Email</h3>
+          <p>
+            <a href="mailto:sharontokiti@gmail.com">sharontokiti@gmail.com</a>
+          </p>
+        </div>
+      </div>
+      
+      {/* Welcome Message */}
+      <div className="welcome-message">
+        <p>Please feel free to reach out to me for any work or collaboration using the form below.</p>
+      </div>
+      
+      {/* Contact Form */}
+      <div className="contact-form">
+        <h3>Contact Form</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" rows="4" required />
+          </div>
+
+          <button type="submit">Send</button>
+        </form>
+      </div>
+      
+      {/* Social Media Handles */}
+      <div className="social-media-handles">
+        <ul className="social-media-list">
+          <li>
+            <a href="https://www.linkedin.com/sharonmshekero">
+              <i className="bi bi-linkedin"></i> LinkedIn
+            </a>
+          </li>
+    
+          <li>
+            <a href="https://www.github.com/yourgithubprofile">
+              <i className="bi bi-github"></i> GitHub
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/yourtwitterprofile">
+              <i className="bi bi-twitter"></i> Twitter
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/yourtwitterprofile">
+              <i className="bi bi-medium"></i> Medium
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
+
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,16 @@ import Resume from './Resume';
 import Footer from './Footer';
 
 const Home = () => {
+  const handleDownloadCV = () => {
+    // Replace this with the actual URL of your CV file
+    const cvUrl = 'https://drive.google.com/file/d/11Bz92Mgm_1CqP193gXOMTAS9lAvBgn4w/view?usp=sharing';
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = cvUrl;
+    downloadLink.target = '_blank'; // Open in a new tab
+    downloadLink.download = 'sharon_cv.pdf'; // Set the desired file name
+    downloadLink.click();
+  };
   return (
     <div className='page-container'>
     <div className="home-content">
@@ -11,7 +21,7 @@ const Home = () => {
           <h1 className='home-title'>Hello,  my name is Sharon Tokiti</h1>
           <p> I'm thrilled to welcome you to my portfolio and I am very eager to contribute my skills and knowledge in a dynamic professional environment.</p>
           <h3 className='home-profession'>FULL STACK DEVELOPER AND A TEACHER!</h3>
-          <button className='button-cv'>Download CV</button>
+          <button className='button-cv' onClick={handleDownloadCV}>Download CV</button>
        </div>
         <div className="home-img-container">
           <img src='/sharonhome.jpg' alt='sharon photo' className='home-img'/>
